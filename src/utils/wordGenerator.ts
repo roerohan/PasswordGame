fcf1c2b195b10965c6704512c6ddfb1ce91f09eb
Arushi Jain <arushijain1479@gmail.com>
2020-05-03 22:12:26 +0530

    feat: Added wordGenerator

---
+++ src/utils/wordGenerator.ts
@@ -0,0 +1,8 @@
+export default function wordGenerator():string {
+    const words:string[] = ['word1', 'word2'];
+    function randomInteger(min:number, max:number): number {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    const randomWord:string = words[randomInteger(0, words.length)];
+    return randomWord;
+}
