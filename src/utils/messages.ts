33dad3c6e60b5df8812162d770cfa296dd98b932
Rohan Mukherjee <rohanbayern@gmail.com>
2020-05-04 02:37:30 +0530

    feat: Added gameEnded message

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -2,6 +2,7 @@ export default {
     serverError: 'serverError',
     gameNotFound: 'gameNotFound',
     gameStarted: 'gameStarted',
+    gameEnded: 'gameEnded',
     noPlayers: 'noPlayers',
     incorrect: 'incorrect',
     correct: 'correct',
