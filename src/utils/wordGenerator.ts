95916cb8c36b59c383207ce7a52dc10332811b55
Rohan Mukherjee <rohanbayern@gmail.com>
2020-05-04 00:01:23 +0530

    fix: Return numbers less than max

--- src/utils/wordGenerator.ts
+++ src/utils/wordGenerator.ts
@@ -1,8 +1,9 @@
 export default function wordGenerator(): string {
     const words: string[] = ['word1', 'word2'];
+
     function randomInteger(min: number, max: number): number {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        return Math.floor(Math.random() * (max - min)) + min;
     }
-    const randomWord: string = words[randomInteger(0, words.length)];
-    return randomWord;
+
+    return words[randomInteger(0, words.length)];
 }
