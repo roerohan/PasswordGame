30de1dbfc6abb123a72cea5bf5c5c749b867a453
Arushi Jain <arushijain1479@gmail.com>
2020-05-04 16:04:52 +0530

    fix:

--- src/routes/room.ts
+++ src/routes/room.ts
@@ -1,13 +1,20 @@
 import express from 'express';
 import cryptoRandomString from 'crypto-random-string';
 import { Game, Player } from '../models/models';
+import messages from '../utils/messages';
 
+
+const { ROUNDS } = process.env;
 
 const router = express.Router();
 
 router.get('/create', async (req: express.Request, res: express.Response) => {
-    const { username, access, rounds } = req.query;
-    const roomId: string = await cryptoRandomString({ length: 6 });
+    const { username, access } = req.query;
+    let { rounds } = req.query;
+    if (!rounds) {
+        rounds = ROUNDS;
+    }
+    const roomId: string = cryptoRandomString({ length: 6 });
     const player = new Player({
         username,
     });
@@ -18,19 +25,38 @@ router.get('/create', async (req: express.Request, res: express.Response) => {
         rounds,
     });
     await game.save();
-    res.json({ success: true, message: 'roomCreated' });
+    if (!game) {
+        res.json({ success: false, message: messages.serverError });
+    } else {
+        res.json({ success: true, message: messages.roomCreated });
+    }
 });
 
 router.get('/join/:roomId', async (req: express.Request, res: express.Response) => {
     const { username } = req.query;
-    const { roomId } = req.params;
+    let { roomId } = req.params;
+    if (!roomId) {
+        const n: number = await Game.count({ access: 'public' });
+        const r: number = Math.floor(Math.random() * n);
+        const element = await Game.find({ access: 'public' }).limit(1).skip(r);
+        roomId = element[0].roomId;
+    }
+    if (!username) {
+        res.json({ success: false, message: messages.usernameNotSet });
+        return;
+    }
     const player = new Player({
         username,
     });
     const game = await Game.findOne({ roomId });
+    if (!game) {
+        res.json({ success: false, message: messages.gameNotFound });
+        return;
+    }
+
     game.players.push(player);
-    await Game.findOneAndUpdate({ roomId }, { players: game.players });
-    res.json({ success: true, message: 'playerAdded' });
+    await game.save();
+    res.json({ success: true, message: messages.playerAdded });
 });
 
 
