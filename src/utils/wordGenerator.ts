2169eee99341b7f51ac663d1a6fe36a8bfc0551e
Rohan Mukherjee <rohanbayern@gmail.com>
2020-05-04 17:52:34 +0530

    feat: Add words to wordlist for testing

--- src/utils/wordGenerator.ts
+++ src/utils/wordGenerator.ts
@@ -1,5 +1,15 @@
 export default function wordGenerator(): string {
-    const words: string[] = ['word1', 'word2'];
+    const words: string[] = [
+        'word1',
+        'word2',
+        'word3',
+        'word4',
+        'word5',
+        'word6',
+        'word7',
+        'word8',
+        'word9',
+    ];
 
     function randomInteger(min: number, max: number): number {
         return Math.floor(Math.random() * (max - min)) + min;
