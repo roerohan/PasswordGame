6163ec850802e94e03adac74d05fc21ad8722345
Arushi Jain <arushijain1479@gmail.com>
2020-05-05 12:43:09 +0530

    fix: Player checking

--- src/routes/room.ts
+++ src/routes/room.ts
@@ -18,6 +18,11 @@ router.post('/create', async (req: express.Request, res: express.Response) => {
         return;
     }
 
+    if (!(/^[a-zA-Z0-9._-]{3,20}$/i.test(username))) {
+        res.json({ success: false, message: messages.usernameInvalid });
+        return;
+    }
+
     const roomId: string = cryptoRandomString({ length: 6 });
     const player = new Player({
         username,
@@ -52,15 +57,29 @@ router.get('/join/:roomId', async (req: express.Request, res: express.Response) 
         return;
     }
 
-    const player = new Player({
-        username,
-    });
+    if (!(/^[a-zA-Z0-9._-]{3,20}$/i.test(username.toString()))) {
+        res.json({ success: false, message: messages.usernameInvalid });
+        return;
+    }
 
     const game = await Game.findOne({ roomId });
     if (!game) {
         res.json({ success: false, message: messages.gameNotFound });
         return;
     }
+    let i;
+    for (i = 0; i < game.players.length; i += 1) {
+        if (game.players[i].username === username) {
+            res.json({ success: false, message: messages.usernameAlreadyExists });
+            return;
+        }
+    }
+
+
+    const player = new Player({
+        username,
+    });
+
 
     game.players.push(player);
     game.markModified('players');
