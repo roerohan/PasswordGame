8e9b70169c26565b00aeb899d0ec723a544e5fdc
Rohan Mukherjee <rohanbayern@gmail.com>
2020-05-07 15:04:33 +0530

    fix: Message model separated from Chat

--- src/models/chat.ts
+++ src/models/chat.ts
@@ -1,25 +1,33 @@
 import mongoose from 'mongoose';
 
+export const messageSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+});
+
 export default new mongoose.Schema({
     roomId: {
         type: String,
         required: true,
+        index: true,
     },
-    messages: new mongoose.Schema({
-        username: {
-            type: String,
-            required: true,
-        },
-        message: {
-            type: String,
-            required: true,
-        },
-    }),
+    messages: [messageSchema],
 }, {
     timestamps: true,
 });
 
+export interface MessageInterface extends mongoose.Document {
+    username: string;
+    message: string;
+}
+
 export interface ChatInterface extends mongoose.Document {
     roomId: string;
-    messages: { username: string, message: string };
+    messages: MessageInterface[];
 }
